ba5b0088c567dda28a9a2dbc34a0aae7b8780a2e
l797l <ali.mahdi.67.99@gmail.com>
2026-06-15 04:38:53 +0300

    Add DeSet to moduls

--- WebApplication2/DBContext/AppDBContext.cs
+++ WebApplication2/DBContext/AppDBContext.cs
@@ -1,9 +1,14 @@
 
 using Microsoft.EntityFrameworkCore;
+using WebApplication2.DBContext.Modules;
 
 public class AppDBContext: DbContext
 {
     public AppDBContext(DbContextOptions<AppDBContext> options): base(options) {}
-    
+    public DbSet<Users>  Users { get; set; }
+    public DbSet<Product>  Products { get; set; }
+    public DbSet<Orders>  Orders { get; set; }
+    public DbSet<OrderProduct> OrderProducts { get; set; }
+    public DbSet<ImgProduct>ImgProducts { get; set; }
     
 }
